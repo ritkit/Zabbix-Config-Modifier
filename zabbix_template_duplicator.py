82926d80933629dc1ecb88af8494c3bbb7caf744
Rahl, Christian <contact@christianrahl.com>
2024-08-12 11:32:00 -0300

    New Duplicator Work

--- zabbix_template_duplicator.py
+++ zabbix_template_duplicator.py
@@ -1,9 +1,8 @@
 #!/bin/python
 
-"""
 
-"""
-
+import argparse
+from zabbix_config_file_processor import ConfigFile, YamlPath
 
 class TemplateDuplicator:
     """
@@ -14,4 +13,18 @@ class TemplateDuplicator:
     def __init__(self, infile: str, outfile: str) -> None:
         self.zabbix_config_file = ConfigFile(infile, outfile)
 
-    
+    def duplicator(self, src_path: YamlPath, dst_path: YamlPath, **args):
+        """ Do the duplicating of a zabbix template item into another location.
+        """
+
+        
+
+        #Process item being copied, update UUID, update name...
+
+        #Copy item into 
+
+
+
+if __name__ == "__main__":
+    parse = argparse("Duplicate a zabbix template item into another location. This can be inside \
+                     the same parent template, or into another template")
